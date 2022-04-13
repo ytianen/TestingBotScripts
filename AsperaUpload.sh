da369c824d26f12f16f08f563bbab33531cb8824
Tian Yu <ytianen@gmail.com>
2022-04-13 13:01:30 -0700

    Update AsperaUpload.sh

--- AsperaUpload.sh
+++ AsperaUpload.sh
@@ -1,31 +1,21 @@
 osascript <<EOD
-    on run
-    tell application "Aspera Connect"
-	        activate
-	        tell application "System Events"
-	            tell process "Aspera Connect"
-			        repeat until exists of button 1 of window 1
-			        delay 1
-			        end repeat
-			        delay 2
-			        keystroke return
-			        click button "Yes"
-                end tell
+tell application "Aspera Connect"
+	activate
+	tell application "System Events"
+		tell process "Aspera Connect"
+			repeat until exists button "Yes" of window 1
+				delay 1
+			end repeat
+			delay 1
+			click checkbox of window 1
+			delay 1
+			click button "Yes" of window 1
             end tell
         end tell
-        tell application "Aspera Connect"
-	        activate
-	        tell application "System Events"
-	            tell process "Aspera Connect"
-	                repeat until exists window 1
-	                delay 1
-	                end repeat
-		     end tell
-            end tell
-        end tell
+end tell
         
 -- Make a selection from the popupbutton.
-delay 2.508544
+delay 5
 set timeoutSeconds to 2.000000
 set uiScript to "click pop up button \"Where:\" of window 1 of application process \"IBM Aspera Connect\""
 my doWithTimeout( uiScript, timeoutSeconds )
@@ -51,7 +41,7 @@ my doWithTimeout( uiScript, timeoutSeconds )
 -- Mouse Clicked (select file)
 delay 3
 set timeoutSeconds to 2.000000
-set uiScript to "click group 2 of list 1 of scroll area 4 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process \"IBM Aspera Connect\""
+set uiScript to "click group 1 of list 1 of scroll area 4 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process \"IBM Aspera Connect\""
 my doWithTimeout( uiScript, timeoutSeconds )
 
 -- Click the “Open” button.
