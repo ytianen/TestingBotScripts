f4ac6d13d4c20670e83f3df368cddb57b72eedde
Tian Yu <ytianen@gmail.com>
2022-04-13 13:17:10 -0700

    Update AsperaUpload.sh

--- AsperaUpload.sh
+++ AsperaUpload.sh
@@ -4,6 +4,7 @@
 # sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
 
 osascript <<EOD
+
 tell application "Aspera Connect"
 	activate
 	tell application "System Events"
@@ -15,54 +16,20 @@ tell application "Aspera Connect"
 			click checkbox of window 1
 			delay 1
 			click button "Yes" of window 1
+			delay 5
 			repeat until exist button "Cancel" of window 1
 				delay 1
 			end repeat
 			click pop up button "Where:" of window 1 of application process "Aspera Connect"
-			delay 5
+			delay 3
 			click menu item "Users" of menu 1 of pop up button "Where:" of window 1 of application process "Aspera Connect"
 			delay 5
 			click group 1 of list 1 of scroll area 2 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process "IBM Aspera Connect"
 			delay 5
+			click UI Element "Open" of window 1 of application process "Aspera Connect"
         	end tell
         end tell
 end tell
-        
--- Make a selection from the popupbutton.
-delay 5
-set timeoutSeconds to 2.000000
-set uiScript to "click pop up button \"Where:\" of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
-
--- Users
-delay 3
-set timeoutSeconds to 2.000000
-set uiScript to "click menu item \"Users\" of menu 1 of pop up button \"Where:\" of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
-        
--- Click Username (selenium)
-delay 3
-set timeoutSeconds to 2.000000
-set uiScript to "click group 3 of list 1 of scroll area 2 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
-
--- Mouse Clicked (desktop)
-delay 3
-set timeoutSeconds to 2.000000
-set uiScript to "click group 3 of list 1 of scroll area 2 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
-
--- Mouse Clicked (select file)
-delay 3
-set timeoutSeconds to 2.000000
-set uiScript to "click group 1 of list 1 of scroll area 4 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
-
--- Click the “Open” button.
-delay 1.822998
-set timeoutSeconds to 2.000000
-set uiScript to "click UI Element \"Open\" of window 1 of application process \"IBM Aspera Connect\""
-my doWithTimeout( uiScript, timeoutSeconds )
 
 EOD
 
