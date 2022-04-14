400d57e302ed1d4b40561f5a7959f45f416c203b
Tian Yu <ytianen@gmail.com>
2022-04-14 12:41:20 -0700

    Update AsperaUpload.sh

--- AsperaUpload.sh
+++ AsperaUpload.sh
@@ -2,9 +2,13 @@
 # sudo hdiutil attach  http://download.asperasoft.com/download/sw/connect/3.6.6/AsperaConnectInstaller-3.6.6.119698.dmg
 # sudo installer -package /Volumes/Aspera\ Connect\ Installer/AsperaConnectInstaller.pkg -target /
 # sudo hdiutil detach /Volumes/Aspera\ Connect\ Installer/
+
+MyValue='/Users/selenium/Desktop/Logo.jpg'
+
 osascript <<EOD
 tell application "Aspera Connect"
 	activate
+	set myFile to do shell script "echo '$MyValue'"
 	tell application "System Events"
 		tell process "Aspera Connect"
 			repeat until exists button "Yes" of window 1
@@ -22,17 +26,13 @@ tell application "Aspera Connect"
 				delay 1
 			end repeat
 			delay 2
-			click menu item "Untitled" of menu 1 of pop up button "Where:" of window 1
-			delay 5
-     			click group 4 of list 1 of scroll area 2 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1
+			keystroke "G" using {command down, shift down}
 			delay 2
-     			click group 1 of list 1 of scroll area 3 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1
+			keystroke myFile
 			delay 2
-     			click group 3 of list 1 of scroll area 4 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1
+			keystroke return
 			delay 2
-     			click group 1 of list 1 of scroll area 5 of scroll area 1 of browser 1 of splitter group 1 of splitter group 1 of window 1
-			delay 2
-			click button "Open" of window 1
+			keystroke return
 		end tell
 	end tell
 end tell
