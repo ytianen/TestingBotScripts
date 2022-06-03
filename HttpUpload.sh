3733710fdef9f1f2dc8173351b36ec3fd367f5cc
Tian Yu <ytianen@gmail.com>
2022-06-03 10:45:12 -0700

    Update HttpUpload.sh

--- HttpUpload.sh
+++ HttpUpload.sh
@@ -1,6 +1,6 @@
 tell application "System Events"
 	tell application process "Chrome"
-		repeat until exists button "Open" of sheet 1 of window 1
+		repeat until exists button "Open" of window 1
 			delay 1
 		end repeat
 		keystroke "G" using {command down, shift down}
