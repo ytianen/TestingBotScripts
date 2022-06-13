0a6845b76d63a99e29d0f676b644d6a0943fa39b
Tian Yu <ytianen@gmail.com>
2022-06-13 09:51:11 -0700

    Update HttpUpload.sh

--- HttpUpload.sh
+++ HttpUpload.sh
@@ -9,7 +9,10 @@ tell application "System Events"
 		repeat until exists UI element 1 of row 3 of outline 1 of scroll area 1 of splitter group 1 of sheet 1 of window 1
 			delay 1
 		end repeat
-		display dialog "found window for chrome"
+		# display dialog "found window for chrome"
+	end tell
+	tell application "Dock"
+		click UI Element "Google Chrome" of list 1 of process 1
 	end tell
 	tell application process "Chrome"
 		set frontmost to true
